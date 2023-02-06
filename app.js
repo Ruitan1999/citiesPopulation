413800082f533af70268b125804476c488d0ecad
Ruitan Huang <ruitan1520@gmail.com>
2023-02-06 14:19:56 +0800

    main

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ const options = {
 	}
 };
 
+
+const getCountryName = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '230234f211msh8dd6d8c2374c270p164240jsnad194c9e1b6a',
+        'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
+    }
+};
+
+
 const tableCtn = document.querySelector('.trtr');
 const userInput = document.querySelector('input');
 const btn = document.querySelector('button');
 const hideHeaders = document.querySelector('.container')
 const loader = document.querySelector('#loading');
+const countryName = document.querySelector('.countryName');
+const text = document.querySelector('.confirmedCase');
+const matchList = document.querySelector('#match-list');
+
 let cityData
+
 
 function displayLoading() {
     hideHeaders.classList.remove('display');
@@ -36,6 +51,44 @@ function hideLoading() {
 
 let params = ''
 
+function searchStates (searchText) {
+    fetch('https://covid-19-statistics.p.rapidapi.com/reports', getCountryName)
+	.then(response => response.json())
+	.then(function countryNames (items) {
+        let matches = items.data.filter(item => {
+            const regex= new RegExp(`^${searchText}`, 'gi');
+            return item.region.name.match(regex);
+        });
+            if(searchText.length === 0) {
+                matches = [];
+                matchList.innerHTML = '';
+            }
+          
+
+            outputHtml(matches);
+        });
+}
+
+     //show results in HTMl
+     const outputHtml = matches => {
+        if(matches.length > 0) {
+            const html = matches.map(match => 
+                `
+            <div class="card card-body mb-1">
+            <button>${match.region.name} </button>
+            </div>
+            `).join(''); 
+            matchList.innerHTML = html;
+
+        }
+
+       
+    }
+
+    
+
+    
+
 const callParams = () => {
     params = userInput.value 
     displayLoading();
@@ -43,22 +96,28 @@ const callParams = () => {
     .then(response => response.json())
     .then(function diplayItems (items) {
         cityData = items.data
-        let tableData = "";
+        console.log(cityData);
         displayCountry = createRows(items.data);
     })
-    
+    .catch(err => console.error(err));
 }
 
 
 btn.addEventListener('click', callParams)
-userInput.addEventListener('keyup', function(e){
-    if (e.key === "Enter") {
-        callParams();
-    }
-});
+
+// userInput.addEventListener('input', () => searchS
+userInput.addEventListener('input', () => searchStates(userInput.value));
+
+// // userInput.addEventListener('input', () => searchS
+// userInput.addEventListener('keyup', function(e){
+//     if (e.key === "Enter") {
+//         callParams();
+//     }
+// });
 
 function createRows (items) {
     displayCountry = items.map(function(item){
+        console.log(item.region.name)
         // sortCounfirm ()
         return `
     <tr>
@@ -77,23 +136,35 @@ function createRows (items) {
         tableCtn.innerHTML = displayCountry;
 }
 
-function sortColumn (columnName) {
-    const dataType = typeof cityData[0][columnName];
-    
-    sortDirection =  !sortDirection; 
 
-    switch(dataType) {
-        case `number`:
-            sortNumbeColumn(sortDirection, columnName);
-            break;
-    }
+
+
+function sortColumn (columnName) {
+    const arrowElement = document.getElementById(columnName + "-arrow");
+    sortDirection =  !sortDirection; 
+    arrowElement.innerHTML = sortDirection ? "&#9660" : "&#9650";
     
-    console.log(cityData);
+    const dataType = typeof cityData[0][columnName];
+    switch(dataType) {
+            case `number`:
+            
+            sortNumbeColumn(sortDirection, columnName);
+            break
+    }
+   
     createRows(cityData);
 }
 
 function sortNumbeColumn (sort, columnName) {
     cityData = cityData.sort((a, b) => {
-        return sort ? a[columnName] - b[columnName] : b[columnName] - a[columnName]
+        return sort ? a[columnName] - b[columnName] 
+        : 
+        b[columnName] - a[columnName]
+
+
+        
     });
 }
+
+
+
