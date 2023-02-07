76f286eccaa8141ad7c9c3a3d510b24255c4fa36
Ruitan Huang <ruitan1520@gmail.com>
2023-02-07 11:33:26 +0800

    main

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let sortDirection = false;
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '230234f211msh8dd6d8c2374c270p164240jsnad194c9e1b6a',
-		'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '230234f211msh8dd6d8c2374c270p164240jsnad194c9e1b6a',
+        'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
+    }
 };
 
 
@@ -51,59 +51,72 @@ function hideLoading() {
 
 let params = ''
 
-function searchStates (searchText) {
-    fetch('https://covid-19-statistics.p.rapidapi.com/reports', getCountryName)
-	.then(response => response.json())
-	.then(function countryNames (items) {
+function searchStates(searchText) {
+    fetch('https://covid-19-statistics.p.rapidapi.com/reports', getCountryName).then(response => response.json()).then(function countryNames(items) {
         let matches = items.data.filter(item => {
-            const regex= new RegExp(`^${searchText}`, 'gi');
+            const regex = new RegExp(`^${searchText}`, 'gi');
             return item.region.name.match(regex);
         });
-            if(searchText.length === 0) {
-                matches = [];
-                matchList.innerHTML = '';
-            }
-          
 
-            outputHtml(matches);
-        });
-}
+        // function getUniqueContries(countries) {
+        //     let getCountries = items.data;
+        //     matches.forEach(function (item) {
+        //         if (! getCountries.includes(item.region.name)) {
+        //             getCountries.push(item.region.name);
+        //         }
+        //     });
+        //     return getCountries;
+        // }
+        // console.log(getCountries);
 
-     //show results in HTMl
-     const outputHtml = matches => {
-        if(matches.length > 0) {
-            const html = matches.map(match => 
-                `
-            <div class="card card-body mb-1">
-            <button>${match.region.name} </button>
-            </div>
-            `).join(''); 
-            matchList.innerHTML = html;
+        // foreach i as matches
+        // if output !contain i than output .add i
 
+
+        if (searchText.length === 0) {
+            matches = [];
+            matchList.innerHTML = '';
         }
 
-       
+
+        outputHtml(matches);
+    });
+}
+
+// show results in HTMl
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches.map(match => `
+            <div class="resultsCard">
+            <button  class="searchBtn" onclick="userInput.value ='${
+            match.region.name
+        }'">${
+            match.region.name
+        } </button>
+            </div>
+            `).join('');
+        matchList.innerHTML = html;
+
+        const searchBtnClear = document.querySelector('.matches');
+        searchBtnClear.addEventListener('click', function () {
+            matchList.innerHTML = '';
+        })
     }
-
-    
-
-    
-
-const callParams = () => {
-    params = userInput.value 
-    displayLoading();
-    fetch(`https://covid-19-statistics.p.rapidapi.com/reports?region_name=${params}`, options)
-    .then(response => response.json())
-    .then(function diplayItems (items) {
-        cityData = items.data
-        console.log(cityData);
-        displayCountry = createRows(items.data);
-    })
-    .catch(err => console.error(err));
 }
 
 
-btn.addEventListener('click', callParams)
+const callParams = () => {
+    params = userInput.value
+    displayLoading();
+    fetch(`https://covid-19-statistics.p.rapidapi.com/reports?region_name=${params}`, options).then(response => response.json()).then(function diplayItems(items) {
+        cityData = items.data
+        displayCountry = createRows(items.data);
+    }).catch(err => console.error(err));
+}   
+    btn.addEventListener('click', function () {
+    matchList.innerHTML = '';
+    callParams()
+})
 
 // userInput.addEventListener('input', () => searchS
 userInput.addEventListener('input', () => searchStates(userInput.value));
@@ -115,56 +128,60 @@ userInput.addEventListener('input', () => searchStates(userInput.value));
 //     }
 // });
 
-function createRows (items) {
-    displayCountry = items.map(function(item){
+function createRows(items) {
+    displayCountry = items.map(function (item) {
         console.log(item.region.name)
         // sortCounfirm ()
         return `
     <tr>
-        <td>${item.region.province}</td>
-        <td>${item.date}</td>
-        <td>${item.confirmed}</td>
-        <td>${item.deaths}</td>
-        <td>${item.active}</td>
-        <td>${item.fatality_rate}</td>
-        <td>${item.last_update}</td>
+        <td>${
+            item.region.province
+        }</td>
+        <td>${
+            item.date
+        }</td>
+        <td>${
+            item.confirmed
+        }</td>
+        <td>${
+            item.deaths
+        }</td>
+        <td>${
+            item.active
+        }</td>
+        <td>${
+            item.fatality_rate
+        }</td>
+        <td>${
+            item.last_update
+        }</td>
     </tr>          
         `
     });
     displayCountry = displayCountry.join('');
-        hideLoading();
-        tableCtn.innerHTML = displayCountry;
+    hideLoading();
+    tableCtn.innerHTML = displayCountry;
 }
 
 
-
-
-function sortColumn (columnName) {
+function sortColumn(columnName) {
     const arrowElement = document.getElementById(columnName + "-arrow");
-    sortDirection =  !sortDirection; 
+    sortDirection = ! sortDirection;
     arrowElement.innerHTML = sortDirection ? "&#9660" : "&#9650";
-    
+
     const dataType = typeof cityData[0][columnName];
-    switch(dataType) {
-            case `number`:
-            
-            sortNumbeColumn(sortDirection, columnName);
+    switch (dataType) {
+        case `number`: sortNumbeColumn(sortDirection, columnName);
             break
     }
-   
+
     createRows(cityData);
 }
 
-function sortNumbeColumn (sort, columnName) {
+function sortNumbeColumn(sort, columnName) {
     cityData = cityData.sort((a, b) => {
-        return sort ? a[columnName] - b[columnName] 
-        : 
-        b[columnName] - a[columnName]
+        return sort ? a[columnName] - b[columnName] : b[columnName] - a[columnName]
 
 
-        
     });
 }
-
-
-
